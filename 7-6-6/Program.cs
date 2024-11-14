ced7d5d9678b524f7b3fbc8b0ef8ac5764e4c9e6
Giorf81 <rejroberts7@gmail.com>
2024-11-14 10:41:49 +0300

    Добавьте файлы проекта.

---
+++ 7-6-6/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _7_6_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Record<int, string>  record = new Record<int, string>();
+
+            record.Id = 1;
+            record.Value = "mnogo";
+        }
+    }
+
+    class Record<T1, T2>
+    {
+        public T1 Id;
+        public T2 Value;
+
+        public DateTime Date;
+    }
+}
